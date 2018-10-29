71aff4602fe2f631068557e737b130758bac6802
Adam Flett <abflett@yahoo.ca>
2018-10-29 17:01:27 -0500

    Setup project folder stucture.

---
+++ src/ghost-zappers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ghost-zappers.css';
+
+class GhostZappers extends Component {
+  render() {
+    return (
+      <div className="ghost-zappers">
+        Hello
+      </div>
+    );
+  }
+}
+
+export default GhostZappers;
